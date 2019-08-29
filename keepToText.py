0596f99890c6ba63313a47721e052b1befc674ac
Boris Smus <boris@smus.com>
2019-08-28 22:23:40 -0700

    greatly simplified, keeps mtime, supports lists => markdown lists

--- keepToText.py
+++ keepToText.py
@@ -1,39 +1,32 @@
+#!/usr/bin/env python3
+
 from __future__ import print_function
 import sys, glob, os, shutil, zipfile, time, codecs, re, argparse
 from zipfile import ZipFile
-   
+from dateutil.parser import parse, parserinfo
+from html.parser import HTMLParser
+
 class InvalidEncoding(Exception):
     def __init__(self, inner):
         Exception.__init__(self)
         self.inner = str(inner)
-        
-def msg(s):
-    print(s, file=sys.stderr)
-    sys.stderr.flush()
 
-def htmlFileToText(inputPath, outputDir, tag, attrib, attribVal):
-    basename = os.path.basename(inputPath).replace(".html", ".txt")
-    outfname = os.path.join(outputDir, basename)
+def noteToText(outputDir, note):
+    #print('Note "{}" has {} characters, modified on {}.'.format(
+    #    note.new_name(), len(note.text), note.ctime))
+    if len(note.text) == 0:
+      print('Note named "{}" has no characters'.format(note.new_name()))
+    outfname = os.path.join(outputDir, note.new_name())
     try:
-        with codecs.open(inputPath, "r", "utf-8") as inf, codecs.open(outfname, "w", outputEncoding) as outf:
-            html = inf.read()
-            parser = MyHTMLParser(outf, tag, attrib, attribVal)
-            parser.feed(html)
+        with codecs.open(outfname, "w", outputEncoding) as outf:
+            outf.write(note.text)
+        mod_time = time.mktime(note.ctime.timetuple())
+        os.utime(outfname, (mod_time, mod_time))
     except UnicodeEncodeError as ex:
-        msg("Skipping file " + inputPath + ": " + str(ex))
+        print("Skipping file " + inputPath + ": " + str(ex))
     except LookupError as ex:
         raise InvalidEncoding(ex)
-        
-def htmlDirToText(inputDir, outputDir, tag, attrib, attribVal):
-    try_rmtree(outputDir)
-    try_mkdir(outputDir)
-    msg("Building text files in {0} ...".format(outputDir))
-    
-    for path in glob.glob(os.path.join(inputDir, "*.html")):
-        htmlFileToText(path, outputDir, tag, attrib, attribVal)
-        
-    msg("Done.")
-    
+
 def tryUntilDone(action, check):
     ex = None
     i = 1
@@ -42,49 +35,40 @@ def tryUntilDone(action, check):
             if check(): return
         except Exception as e:
             ex = e
-                
         if i == 20: break
-        
         try:
             action()
         except Exception as e:
             ex = e
-            
         time.sleep(1)
         i += 1
-        
-    sys.exit(ex if ex != None else "Failed")          
-        
-def try_rmtree(dir):
-    if os.path.isdir(dir): msg("Removing {0}".format(dir))
+    sys.exit(ex if ex != None else "Failed")
 
-    def act(): shutil.rmtree(dir)        
-    def check(): return not os.path.isdir(dir)        
+def try_rmtree(dir):
+    if os.path.isdir(dir):
+      print("Removing {0}".format(dir))
+
+    def act(): shutil.rmtree(dir)
+    def check(): return not os.path.isdir(dir)
     tryUntilDone(act, check)
-        
+
 def try_mkdir(dir):
     def act(): os.mkdir(dir)
     def check(): return os.path.isdir(dir)
     tryUntilDone(act, check)
-    
-htmlExt = re.compile(r"\.html$", re.I)
-    
-class Note:
-    def __init__(self, heading, text, labels):
 
+htmlExt = re.compile(r"\.html$", re.I)
+
+class Note:
+    def __init__(self, path, heading, text):
+        self.path = path
         self.ctime = parse(heading, parserinfo(dayfirst=True))
         self.text = text
-        self.labels = labels
 
-    def getWsSeparatedLabelString(self):
-        "Return a WS-separated label string suited for import into CintaNotes"
-        labels = []
-        for label in self.labels:
-            label = label.replace(" ", "_")
-            label = label.replace(",", "")
-            labels.append(label)
-        return " ".join(labels)
-        
+    def new_name(self):
+        return os.path.basename(self.path).replace(".html", ".md")
+
+
 def extractNoteFromHtmlFile(inputPath):
     """
     Extracts the note heading (containing the ctime), text, and labels from
@@ -93,28 +77,39 @@ def extractNoteFromHtmlFile(inputPath):
 
     with codecs.open(inputPath, 'r', 'utf-8') as myfile:
         data = myfile.read()
-    
+
+    from lxml import etree
     tree = etree.HTML(data)
 
-    heading = tree.xpath("//div[@class='heading']/text()")[0].strip()
-    text = "\n".join(tree.xpath("//div[@class='content']/text()"))
-    labels = tree.xpath("//div[@class='labels']/span[@class='label']/text()")
+    # Prepend \n to all of the <br> tags to preserve newlines.
+    for br in tree.xpath("*//br"):
+        br.tail = "\n" + br.tail if br.tail else "\n"
 
-    return Note(heading, text, labels)
-        
+    heading = tree.xpath("//div[@class='heading']/text()")[0].strip()
+    text = ''.join(tree.xpath("//div[@class='content']/text()"))
+
+    if not text:
+        # It might be a list. Try to parse out the list items and convert into a
+        # markdown-style list.
+        items = []
+        for li in tree.xpath("*//li/span[@class='text']/text()"):
+            items.append('- ' + li)
+        text = '\n'.join(items)
+
+
+    return Note(inputPath, heading, text)
+
 def processHtmlFiles(inputDir):
     "Iterates over Keep HTML files to extract relevant notes data"
 
-    msg("Processing HTML files in {}".format(inputDir))
-    
+    print("Processing HTML files in {}".format(inputDir))
+
     notes = []
     for path in glob.glob(os.path.join(inputDir, "*.html")):
         try:
             note = extractNoteFromHtmlFile(path)
             notes.append(note)
         except IndexError: pass
-
-    msg("Done.")
 
     return notes
 
@@ -125,40 +120,13 @@ def getHtmlDir(takeoutDir):
         if not os.path.isdir(dir): continue
         htmlFiles = [f for f in os.listdir(dir) if htmlExt.search(f)]
         if len(htmlFiles) > 0: return dir
-        
-def htmlDirToXml(zipFileDir, htmlDir):
-    xmlFile = os.path.join(zipFileDir, "cintanotes.xml")
-    notes = processHtmlFiles(inputDir=htmlDir)
-
-    cintaXml = Template("""
-        <?xml version="1.0"?>
-        <notebook version="4104" uid="">
-
-        %for note in notes:
-                <note uid="" created="${note.ctime.strftime("%Y%m%dT%H%M%S")}"
-                    source="" link="" remarks=""
-                    tags="${note.getWsSeparatedLabelString()}"
-                    section="0" plainText="1">
-                    <![CDATA[${note.text}]]>
-                </note>
-        %endfor
-
-        </notebook>
-    """)
-
-    msg("Generating CintaNotes XML file: " + xmlFile)
-
-    with codecs.open(xmlFile, 'w', 'utf-8') as outfile:
-        outfile.write(cintaXml.render(notes=notes))
 
 def keepZipToOutput(zipFileName):
     zipFileDir = os.path.dirname(zipFileName)
     takeoutDir = os.path.join(zipFileDir, "Takeout")
-    
     try_rmtree(takeoutDir)
-    
     if os.path.isfile(zipFileName):
-        msg("Extracting {0} ...".format(zipFileName))
+        print("Extracting {0} ...".format(zipFileName))
 
     try:
         with ZipFile(zipFileName) as zipFile:
@@ -168,23 +136,23 @@ def keepZipToOutput(zipFileName):
 
     htmlDir = getHtmlDir(takeoutDir)
     if htmlDir is None: sys.exit("No Keep directory found")
-    
-    msg("Keep dir: " + htmlDir)
+    print("Keep dir: " + htmlDir)
 
-    if args.format == "Evernote":
-        htmlDirToText(inputDir=htmlDir,
-            outputDir=os.path.join(zipFileDir, "Text"),
-            tag="div", attrib="class", attribVal="content")
-            
-    elif args.format == "CintaNotes":
-        htmlDirToXml(zipFileDir=zipFileDir, htmlDir=htmlDir)
-        
+    notes = processHtmlFiles(inputDir=htmlDir)
+
+    outputDir = os.path.join(zipFileDir, "Text")
+
+    # Convert each note into a plaintext file with the parsed out creation time.
+    try_mkdir(outputDir)
+    for note in notes:
+        noteToText(outputDir, note)
+
 def setOutputEncoding():
     global outputEncoding
     outputEncoding = args.encoding
     if outputEncoding is not None: return
     if args.system_encoding: outputEncoding = sys.stdin.encoding
-    if outputEncoding is not None: return    
+    if outputEncoding is not None: return
     outputEncoding = "utf-8"
 
 def getArgs():
@@ -194,65 +162,13 @@ def getArgs():
         help="character encoding of output")
     parser.add_argument("--system-encoding", action="store_true",
         help="use the system encoding for the output")
-    parser.add_argument("--format", choices=["Evernote", "CintaNotes"],
-        default='Evernote', help="Output Format")
     global args
-    args = parser.parse_args()    
-
-def doImports():
-    if args.format == "Evernote":
-        global HTMLParser
-        try:
-            from HTMLParser import HTMLParser
-        except ImportError:
-            from html.parser import HTMLParser
-        
-        global MyHTMLParser
-        class MyHTMLParser(HTMLParser):
-            def attrib_matches(self, tag, attrs):
-                return [pair for pair in attrs if
-                        pair[0] == self.attrib and pair[1] == self.attribVal]    
-
-            def handle_starttag(self, tag, attrs):
-                if tag == self.tag:
-                    if self.attrib_matches(tag, attrs) and not self.nesting:
-                        self.nesting = 1
-                    elif self.nesting:
-                        self.nesting += 1
-                elif tag == "br" and self.nesting:
-                    self.outf.write(os.linesep)
-
-            def handle_endtag(self, tag):
-                if tag == self.tag and self.nesting:
-                    self.nesting -= 1
-                    
-            def handle_data(self, data):
-                if self.nesting:
-                    self.outf.write(data.strip())
-            
-            def __init__(self, outf, tag, attrib, attribVal):
-                HTMLParser.__init__(self)
-                self.outf = outf
-                self.tag = tag
-                self.attrib = attrib
-                self.attribVal = attribVal
-                self.nesting = 0
-                
-    elif args.format == "CintaNotes":
-        global etree
-        from lxml import etree
-        global Template
-        from mako.template import Template
-        global parse, parserinfo
-        from dateutil.parser import parse, parserinfo
+    args = parser.parse_args()
 
 def main():
     getArgs()
-    doImports()
     setOutputEncoding()
-        
-    msg("Output encoding: " + outputEncoding)
-    
+
     try:
         keepZipToOutput(args.zipFile)
     except WindowsError as ex:
